92259c6da90573551f7df5d022cf42f4b07d5273
John Anthony Radosta <JohnAntonusMaximus@users.noreply.github.com>
2018-05-23 16:06:22 -0400

    removing sh

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#1/bin/bash
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" -y
-brew install ffmpeg
-sleep 5
-cd $HOME
-ffmpeg -f avfoundation -video_size 1280x720 -framerate 15 -i "0" -vframes 1 out.jpg -y
-python ~/homebrew.py
